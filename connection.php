575a5da358a72a076243ea146c968fe87a7ebb16
w00den <w00den@protonmail.com>
2018-01-15 22:25:40 +0300

    pdo fix

--- connection.php
+++ connection.php
@@ -6,8 +6,10 @@ $dbname = $config['pass'];
 
 try {
     $dbh = new PDO('mysql:host=localhost;dbname='.$dbname, $user, $pass,
-        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-} catch {
-    echo("Can't open the database.");
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    die(json_encode(array('outcome' => true)));
+}
+catch(PDOException $ex){
+  die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
 }
 ?>
