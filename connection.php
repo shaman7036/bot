801555e5e97cd96ade0a63857af2277d16c90e09
w00den <w00den@protonmail.com>
2018-01-15 22:51:45 +0300

    mysql

--- connection.php
+++ connection.php
@@ -8,7 +8,7 @@ var_dump($user);
 var_dump($pass);
 var_dump($dbname);
 try {
-    $dbh = new PDO("mysqli:host=localhost;dbname=$dbname", $user, $pass);
+    $dbh = new PDO("mysql:host=localhost;dbname=$dbname", $user, $pass);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     die(json_encode(array('outcome' => true)));
 }
